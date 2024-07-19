d9079edb7216117dd3955e5a18590f6b9f6f850b
t-sasatani <33111879+t-sasatani@users.noreply.github.com>
2024-07-19 20:37:44 +0900

    made it cmd and matplotlib based instead of tkinter

--- serial_toolbox/gui.py
+++ serial_toolbox/gui.py
@@ -1,157 +1,198 @@
-import customtkinter as ctk
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
+import threading
+import time
 import queue
+import cmd
+import readline  # or for Windows: from pyreadline import Readline
+import logging
+import sys
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 from .interface_core import serial_interface
 from .connect import port_manager
-
-import logging
 from .log_init import log_init
 
-class Application(ctk.CTk):
-    """
-    A Example tkinter application for serial communication.
+class SerialMonitor(cmd.Cmd):
+    intro = 'Welcome to the Serial Monitor. Type help or ? to list commands.\n'
+    prompt = '(serial monitor) '
 
-    Attributes
-    ----------
-    interface : serial_interface
-        The interface to interact with the serial port.
-    left_frame : CTkFrame
-        The Left Frame of the application.
-    right_frame : CTkFrame
-        The Right Frame of the application.
-    entry_text : CTkStringVar
-        The String variable linked to the data entry.
-    data_entry : CTkEntry
-        The entry field for the command.
-    send_button : CTkButton
-        The button to send the command.
-    data_text : CTkText
-        The textbox to display sent and received messages. Incoming data from the serial interface and the sent commands will be displayed in this field.
-    figure : Figure
-        The Figure object for the plot in the right frame. This object is what actually contains the graphical representation of the data.
-    plot : AxesSubplot
-        The subplot in the figure. This is where the data from the serial interface gets plotted.
-    canvas : FigureCanvasTkAgg
-        The canvas on which the figure is drawn. This is a tkinter-compatible canvas that the Figure object draws onto.
-    """
-
-    def __init__(self, interface, plotting : bool = True):
-        """Initialize the Application."""
+    def __init__(self, interface, plotting=True, print_numbers=False):
         super().__init__()
         self.interface = interface
-
-        ctk.set_appearance_mode("Dark")
-
-        self.left_frame = ctk.CTkFrame(self)
-        self.right_frame = ctk.CTkFrame(self)
-
-        self.left_frame.grid(row=0, column=0, sticky='ns')
-        self.right_frame.grid(row=0, column=1, sticky='nsew')
-
-        self.entry_text = ctk.StringVar()
-        self.data_entry = ctk.CTkEntry(self.left_frame, textvariable=self.entry_text)
-        self.data_entry.pack(side=ctk.TOP, fill=ctk.X)
-        self.data_entry.bind('<Return>', lambda event: self.send_command())
-
-        self.send_button = ctk.CTkButton(self.left_frame, text="Send", command=self.send_command)
-        self.send_button.pack(side=ctk.TOP, fill=ctk.X)
-
-        self.data_text = ctk.CTkTextbox(self.left_frame, height=10, width=50)
-        self.data_text.pack(side=ctk.TOP, fill=ctk.BOTH, expand=True)
-
         self.plotting = plotting
+        self.print_numbers = print_numbers  # Flag to control numeric data printing
+        self.data_lock = threading.Lock()
 
-        self.figure = Figure(figsize=(5, 4), dpi=100)
-        self.plot = self.figure.add_subplot(1, 1, 1)
-
-        self.canvas = FigureCanvasTkAgg(self.figure, self.right_frame)
-        self.canvas.get_tk_widget().pack(fill=ctk.BOTH, expand=True)
-        self.data_list = []
+        # Initialize plotting parameters
+        self.traces = []
         self.plot_queue = queue.Queue()
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
-        self.right_frame.rowconfigure(0, weight=1)
-        self.right_frame.columnconfigure(0, weight=1)
+        self.print_queue = queue.Queue()
+        self.window_size = 200
 
-        self.rxd_update()
+        if self.plotting:
+            self.figure, self.ax = plt.subplots()
+            self.animation = FuncAnimation(self.figure, self.update_plot, interval=100)
+            plt.ion()  # Turn on interactive mode
+            self.figure.show()
 
-    def send_command(self):
-        """
-        Fetch the command, send it via the serial interface and update the textbox.
-        Clear the command entry field after sending the command.
-        This function is connected to the 'Send' button and the 'Return' key while typing into the data_entry field.
-        """
-        command = self.entry_text.get()
+        # Start the RXD update thread
+        self.running = True
+        self.update_thread = threading.Thread(target=self.rxd_update)
+        self.update_thread.daemon = True
+        self.update_thread.start()
+
+        # Start the print update thread
+        self.print_thread = threading.Thread(target=self.print_rxd)
+        self.print_thread.daemon = True
+        self.print_thread.start()
+
+    def rxd_update(self):
+        """ Continuously update received data. """
+        while self.running:
+            data_queue_size = self.interface.data_queue.qsize()
+            if data_queue_size > 0:
+                for _ in range(data_queue_size):
+                    data_dict = self.interface.data_queue.get()
+
+                    if self.interface.format == 'HEX':
+                        self.print_queue.put("RXD: 0x" + data_dict['data'].hex())
+                    elif self.interface.format == 'STR':
+                        data_dict['data'] = data_dict['data'].strip()
+                        if self.is_comma_separated_numbers(data_dict['data']):
+                            values = [float(x) for x in data_dict['data'].split(',')]
+                            with self.data_lock:
+                                self.update_traces(values)
+                            if self.print_numbers:  # Check the flag before printing
+                                self.print_queue.put("RXD: " + data_dict['data'])
+                        else:
+                            self.print_queue.put("RXD: " + data_dict['data'])
+            time.sleep(0.1)
+
+    def is_comma_separated_numbers(self, data_str):
+        """ Check if the given string is a comma-separated string of numbers. """
+        try:
+            [float(x) for x in data_str.split(',')]
+            return True
+        except ValueError:
+            return False
+
+    def update_traces(self, values):
+        """ Update the traces with new values. """
+        while len(self.traces) < len(values):
+            self.traces.append([])
+        
+        for i, value in enumerate(values):
+            self.traces[i].append(value)
+        
+        for trace in self.traces:
+            if len(trace) > self.window_size:
+                trace.pop(0)
+
+    def update_plot(self, frame):
+        """ Update the plot with the received data """
+        with self.data_lock:
+            self.ax.clear()
+            for trace in self.traces:
+                self.ax.plot(trace)
+            self.ax.relim()
+            self.ax.autoscale_view()
+            self.figure.canvas.draw()
+            self.figure.canvas.flush_events()
+
+    def print_rxd(self):
+        """ Print the received data without interrupting the CLI. """
+        while self.running:
+            try:
+                message = self.print_queue.get_nowait()
+                # Save the current prompt line
+                current_input = readline.get_line_buffer()
+                saved_line_pos = readline.get_begidx()
+                # Clear the prompt line
+                sys.stdout.write('\r' + ' ' * (len(self.prompt) + len(current_input)) + '\r')
+                sys.stdout.flush()
+                # Print the new message
+                print(message)
+                # Restore the prompt line
+                sys.stdout.write(self.prompt + current_input)
+                sys.stdout.flush()
+                readline.redisplay()
+            except queue.Empty:
+                pass
+            time.sleep(0.1)
+
+    def do_send(self, arg):
+        "Send a command: send <command>"
+        command = arg.strip()
         if self.interface.format == 'STR':        
-            self.data_text.insert(0., "TXD: " + command + "\n")
+            print("TXD: " + command)
         elif self.interface.format == 'HEX':
             try:
                 _ = bytes.fromhex(command)
             except ValueError:
-                print('\'' + command + '\' includes non-hexadecimal number')
-                self.data_text.insert(0., 'ERR: non-hex cmd')
-                self.entry_text.set("")
+                print(f"'{command}' includes non-hexadecimal number")
                 return
-            self.data_text.insert(0., "TXD: 0x" + command + "\n")
+            print("TXD: 0x" + command)
+
         self.interface.write_to_port(command)
-        self.entry_text.set("")
 
-    def rxd_update(self):
-        """
-        Update the plot with the data from the interface. 
-        This function re-plots the data from the serial interface, then schedules itself to be called again after 100 ms.
-        This function is automatically triggered in the initialization of the Application class, implementing a regular update of the plot.
-        """
-        window_size = 200
-        data_queue_size = self.interface.data_queue.qsize()
-        if data_queue_size > 0:
-            self.plot.clear()
-            
-            for _ in range(data_queue_size):
-                data_dict = self.interface.data_queue.get()
+    def do_plot(self, arg):
+        "Show the plot window"
+        if self.plotting:
+            plt.show()
 
-                if self.interface.format == 'HEX':
-                    self.data_text.insert(0., "RXD: 0x" + data_dict['data'].hex() + "\n")
-                elif self.interface.format == 'STR':
-                    data_dict['data'] = data_dict['data'].strip()
-                    if data_dict['data'].strip().isdigit() and self.plotting:
-                        self.plot_queue.put(data_dict) # pass down numbers to plot_queue
-                    else:
-                        self.data_text.insert(0., data_dict['data'] + "\n")
+    def do_exit(self, arg):
+        "Exit the serial monitor"
+        print('Exiting Serial Monitor.')
+        self.running = False
+        return True
 
-            # Append new data points to self.data_list and maintain only the latest window_size data points
-            plot_queue_size = self.plot_queue.qsize()
-            for _ in range(plot_queue_size):
-                data_dict = self.plot_queue.get()
-                self.data_list.append(float(data_dict['data']))  # Store individual data-points
-                if len(self.data_list) > window_size:
-                    self.data_list.pop(0)  # Remove oldest data point to keep size at window_size
-            
-            # Plot the entire data_list which now contains at most window_size data points
-            self.plot.plot(self.data_list)
-            self.canvas.draw()
+    def cmdloop(self, intro=None):
+        """A replacement for the standard cmdloop with additional processing."""
+        self.preloop()  # Hook after initialization before looping
+        if intro is not None:
+            self.intro = intro
+        if self.intro:
+            self.stdout.write(str(self.intro) + "\n")
+        stop = None
+        while self.running:
+            if self.cmdqueue:
+                line = self.cmdqueue.pop(0)
+            else:
+                line = input(self.prompt)
+            try:
+                stop = self.onecmd(line)
+            except Exception as e:
+                print('Exception:', e)
+            if stop:
+                break
+        self.postloop()  # Hook before exiting
+    
+    def preloop(self):
+        """Hook method executed once when the cmdloop() method is called."""
+        pass
 
-        self.after(100, self.rxd_update)
-        
+    def postloop(self):
+        """Hook method executed once when the cmdloop() method is about to return."""
+        self.running = False
 
-def serial_monitor_gui():
+def serial_monitor_cli():
     """
-    Start and run the customtkinter application. 
-    This is the main entry point of the application that creates an instance of the Application class and executes the main loop.
+    Start and run the CLI application. 
+    This is the main entry point of the application that creates an instance of the SerialMonitor class and runs the cmd loop.
     """
     logger = log_init()
 
     port_interface = port_manager.select_port(interactive=True, portname="serial monitor", logger=logger)
     format_input = input("format ('STR', 'HEX') ['STR'] >> ")
-    if format_input.strip():
-        format = format_input
-    else:
-        format = 'STR'
+    format = format_input.strip() if format_input.strip() else 'STR'
+    
+    print_numbers_input = input("Print numeric data to CLI? (y/n) [n] >> ").strip().lower()
+    print_numbers = True if print_numbers_input == 'y' else False
 
     if not port_interface:
         return
     
     target_serial_interface = serial_interface(port_interface, terminal=False, max_queue_size=10, format=format, logger=logger)
-    app = Application(target_serial_interface, plotting = (format == 'STR'))
-    app.mainloop()
+    SerialMonitor(target_serial_interface, plotting=(format == 'STR'), print_numbers=print_numbers).cmdloop()
+
+if __name__ == "__main__":
+    serial_monitor_cli()
